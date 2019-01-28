4490ff50c910883b0a1fbc19b31d45e6495a2ee7
George Zogopoulos-Papaliakos <tailwhipx4@gmail.com>
2019-01-28 20:40:16 +0200

    Set Travis setup to use docker

--- tools/scripts/install_deps.sh
+++ tools/scripts/install_deps.sh
@@ -6,6 +6,3 @@ set -e
 cd /home/ros/catkin_ws
 rosdep update
 rosdep install --from-paths src --ignore-src -r -y
-
-ls -al /home/ros/catkin_ws/src
-ls -al /home/ros/catkin_ws/devel
--- tools/scripts/install_prereqs.sh
+++ tools/scripts/install_prereqs.sh
@@ -6,11 +6,12 @@ set -e
 apt-get -y -q update
 # apt-get -y install apt-utils && \
 apt-get -y -q install sudo vim git wget
-apt-get -y -q install ros-${ROS_DISTRO}-gazebo-ros-pkgs
 
 # Install gazebo and its dependencies
 # Choose version depending on ROS version
+apt-get -y -q install ros-${ROS_DISTRO}-gazebo-ros-pkgs
 
+# # Separate installation of Gazebo from repo
 # # Unneeded, installed with gazebo-ros-pkgs
 # case $ROS_DISTRO in
 #     'kinetic')
@@ -25,23 +26,3 @@ apt-get -y -q install ros-${ROS_DISTRO}-gazebo-ros-pkgs
 #         echo ">>> Unsupported ROS version: " $ROS_DISTRO
 #         ;;
 # esac
-
-# # Create rosdep registry
-# source /opt/ros/${ROS_DISTRO}/setup.bash
-# rosdep init
-    
-# if [$ROS_DISTRO = "kinetic"]
-# then
-#     apt-get install gazebo7
-# elif [$ROS_DISTRO = "melodic"]
-# then
-#     apt-get install gazebo9
-# else
-#     echo "Unsupported ROS version: " $ROS_DISTRO
-# fi
-
-# RUN sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list' && \
-#     wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add - && \
-#     apt-get update && \
-
-    # apt-get install -y libignition-math4-dev
